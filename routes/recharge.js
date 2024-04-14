fbe00e9e6a1cadfa71238e77667aa64c67dd5ac2
dheeraj singh <dheerajsingh@dheerajs-MacBook-Pro.local>
2024-04-14 15:09:02 +0530

    testing

--- routes/recharge.js
+++ routes/recharge.js
@@ -103,41 +103,31 @@ router.post('/approveTransaction', authenticate, async (req, res) => {
         console.log(req.body)
         console.log(req.user)
 
-
         let user = await User.findOne({_id: req.user.id}).session(session);
         let recharge = await Recharge.findOne({transactionId: req.body.transactionId});
 
-        if(recharge.status != "PENDING"){
+        if(recharge.status !== "PENDING" || user.profile!=="ADMIN"){
             throw error("can not approve this recharge");
+        }else{
+            recharge.status = "SUCCESS";
+            recharge.approvedBy = user._id;
+            let receiver = await User.findOne({_id: recharge.userId});
+            receiver.balance += recharge.amount;
+
+            console.log(recharge)
+            await recharge.save();
+            await receiver.save();
+            await session.commitTransaction();
+            await session.endSession();
+            res.json({
+                userId: receiver.username,
+                transactionId: recharge.transactionId,
+                status: recharge.status,
+                amount: recharge.amount
+            })
         }
 
-        recharge.status = "SUCCESS";
-        recharge.approvedBy = user._id;
-        let receiver = await User.findOne({_id: recharge.userId});
-        receiver.balance += recharge.amount;
-
-
-
-    
-
-        console.log(recharge)
-
-        await recharge.save();
-
-        await receiver.save();
-
-
-
-        await session.commitTransaction();
-
-        await session.endSession();
-
-        res.json({
-            userId: receiver.username,
-            transactionId: recharge.transactionId,
-            status: recharge.status,
-            amount: recharge.amount
-        })
+        
     } catch (error) {
 
 
@@ -145,7 +135,7 @@ router.post('/approveTransaction', authenticate, async (req, res) => {
         await session.endSession();
 
         console.log(error)
-        res.status(500).json({ message: 'Error while approving the transaction' });
+        res.status(500).json({ errorMessage: 'Error while approving the transaction' });
     }
 })
 
